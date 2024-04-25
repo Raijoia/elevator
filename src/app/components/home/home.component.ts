b7762214036ba9d986dc6c998a133b87b1d6f92c
Raí Joia <megac154@gmail.com>
2024-04-25 19:42:50 -0300

    feat: stylized home component and structure

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,17 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  links = [
+    { label: 'Candy Machine', url: 'candy-machine' },
+    { label: 'Elevator', url: 'elevator' },
+  ]
 }
