7a983748d6d83e16928efdac6860bc59d0222684
Raí joia miquilino valencio <79546241+Raijoia@users.noreply.github.com>
2024-04-25 20:09:47 +0000

    raw: update project for elevator

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,20 @@
 import { Routes } from '@angular/router';
 import { InitialPageComponent } from './components/initial-page/initial-page.component';
 import { AutomatoComponent } from './components/automato/automato.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'candy-machine',
     component: InitialPageComponent,
     pathMatch: 'full'
   },
   {
     path: 'automato',
     component: AutomatoComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
   }
 ];
