5ee4df8487c0c597b00211a89ac50b4799822abc
Joao Victor Varella Siqueira <joaovvarella@gmail.com>
2024-05-09 09:54:11 -0300

    Primeira versão do HTML -> falta ajustes

---
+++ programming.js
@@ -0,0 +1,26 @@
+var current = 0;
+
+$(document).ready(function(){
+
+  $("#floorSelect li").click(function(){
+  
+    var floor = parseInt($(this).data("floor")),
+     height = floor*25,
+     animate = Math.abs(current-floor)*1000;
+
+
+   if(floor == current) return;
+$("#rightDoor").removeClass("active-right");   
+    $("#leftDoor").removeClass("active-left");  
+    setTimeout(function(){$("#elevatorContainer").css("transition","all "+animate+"ms linear");
+    $("#elevatorContainer").css("bottom",height+"%");
+    current = floor;
+    setTimeout(function(){
+    
+    $("#rightDoor").addClass("active-right");   
+    $("#leftDoor").addClass("active-left");  
+    
+    },animate);},300);
+  });
+
+});
